1b0184fc6b1f60b79f50165049056165f1d4656d
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-18 12:15:20 -0500

    "actions migration file created"

--- data/migrations/20190118120704_action.js
+++ data/migrations/20190118120704_action.js
@@ -1,8 +1,26 @@
 
 exports.up = function(knex, Promise) {
+
+    return knex.schema.createTable('actions', function(tbl) {
+
+        tbl.increments();
+
+        tbl.string('action_name').notNullable();
+
+        tbl.string('action_details');
+
+        tbl.boolean('completed').defaultTo(false);
+
+        tbl.integer('project_id').notNullable().unsigned();
+
+        tbl.foreign('project_id').references('id').on('projects');
+        
+      });
   
 };
 
 exports.down = function(knex, Promise) {
+
+    return knex.schema.dropTableIfExists('actions');
   
 };
