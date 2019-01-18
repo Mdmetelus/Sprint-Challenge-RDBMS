c66898f8f8b292e6a38c79730aba8190b9d9aa5d
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-18 13:04:12 -0500

    project mappers , model created, and som boolean functions

---
+++ data/helpers/mappers.js
@@ -0,0 +1,25 @@
+module.exports = {
+    boolToInt,
+    intToBoolean,
+
+
+
+}
+
+function boolToInt(bool) {
+    return bool === true ? 1 : 0;
+}
+
+function intToBoolean(int) {
+    return int === 1 ? true : false;
+}
+
+function projectToBody(project) {
+    const result = { ...project, completed: intToBoolean(project.completed) }; 
+  
+    if(project.actions) {
+      result.actions = project.actions.map( action => ({...action, completed: intToBoolean(action.completed),
+      }));
+    }
+    return result;
+  }
